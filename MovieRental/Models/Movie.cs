a60a7bb5340d3fe473468729967a5b5bef50b6a8
Sapir <sapir1997@gmail.com>
2019-07-23 16:59:56 +0300

    create models

---
+++ MovieRental/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MovieRental.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Movie Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Producer Name")]
+        [Required]
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public int GenreId { get; set; }
+        [ForeignKey("GenreId")]
+        public Genre Genre { get; set; }
+
+    }
+}
