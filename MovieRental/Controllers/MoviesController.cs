cbdb5d52befab9ec9c1bde505e408ca4c5fc06f5
Amit Hanoch <amit.dvirhanoch@gmail.com>
2019-08-05 18:08:27 +0300

    WIP dropdown

--- MovieRental/Controllers/MoviesController.cs
+++ MovieRental/Controllers/MoviesController.cs
@@ -70,21 +70,21 @@ namespace MovieRental.Controllers
         }
 
         // GET: Movies/Edit/5
-        public async Task<ActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return BadRequest();
             }
 
-            Movie movietoEdit = await _context.Movie.FindAsync(id);
-            if (movietoEdit == null)
+            Movie movieToEdit = await _context.Movie.FindAsync(id);
+            if (movieToEdit == null)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
             }
 
-            ViewBag.GenreId = new SelectList(_context.Genre, "Id", "Name", movietoEdit.GenreId);
-            return View(movietoEdit);
+            ViewBag.GenreId = new SelectList(_context.Genre, "Id", "Name", movieToEdit.GenreId);
+            return View(movieToEdit);
         }
 
         // POST: Books/Edit/5
@@ -92,21 +92,20 @@ namespace MovieRental.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind("Id,Name,ReleaseDate,ProducerId,Price,GenreId")] Movie movieToEdit)
+        public async Task<IActionResult> Edit([Bind("Id,Name,ReleaseDate,ProducerId,Price,GenreId")] Movie movieToEdit)
         {
             if (ModelState.IsValid)
             {
                 if (_context.Movie.AsNoTracking().SingleOrDefault(x => x.MovieId == movieToEdit.MovieId) != null)
                 {
                     _context.Entry(movieToEdit).State = EntityState.Modified;
-                    _context.SaveChanges();
+                    await _context.SaveChangesAsync();
                 }
 
-                return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
             }
 
             ViewBag.GenreId = new SelectList(_context.Genre, "Id", "Name", movieToEdit.GenreId);
-
             return View(movieToEdit);
         }
 
